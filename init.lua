8819f35c8261eda9619c90ac9e2154aa2a56a84b
Ryan Stentz <rbot427@proton.me>
2025-09-02 00:34:38 -0400

    use rustfmt for rust formatting

--- init.lua
+++ init.lua
@@ -779,6 +779,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        rust = { 'rustfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
